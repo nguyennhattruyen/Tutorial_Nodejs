8fb456bcd348f2e73b90596dcc84ac780e6803f5
Nguyen Nhat Truyen <1312643@student.hcmus.edu.vn>
2016-05-16 10:08:20 -0700

    HELLO WORLD

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-	res.send("MÃI YÊU Z MAD");
+	res.send("HELLO WORLD");
 });
 
 app.listen(3000, function (){
