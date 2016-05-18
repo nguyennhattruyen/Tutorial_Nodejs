8e6a991890a33716444d66a3e3dfcfcb2f8d941b
Nguyen Nhat Truyen <1312643@student.hcmus.edu.vn>
2016-05-17 19:52:13 -0700

    ect engine

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require("express");
+var ect = require("ect");
+
 
 var app = express();
+var renderer = ect({
+	root:__dirname + 'views',
+	ext: '.ect'
+});
 
-app.get("/", function(req, res){
-	res.send("HELLO WORLD");
+app.set('view engine','ect');
+app.engine('ect', renderer.render);
+
+app.get("/hello", function(req, res){
+	//res.send("HELLO WORLD "+ req.query.name);
+	res.render('hello', {name: req.query.name})
 });
 
 app.listen(3000, function (){
