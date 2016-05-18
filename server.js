8b01250c67644314cad15af181ba5aa5e44a4f73
Nguyen Nhat Truyen <1312643@student.hcmus.edu.vn>
2016-05-17 21:10:21 -0700

    Calculator

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var ect = require("ect");
+var bodyParser = require('body-parser');
 
 
 var app = express();
@@ -8,6 +9,7 @@ var renderer = ect({
 	ext: '.ect'
 });
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine','ect');
 app.engine('ect', renderer.render);
 
@@ -15,7 +17,18 @@ app.get("/hello", function(req, res){
 	//res.send("HELLO WORLD "+ req.query.name);
 	res.render('hello', {name: req.query.name})
 });
+app.get("/calculator", function(req, res) {
+	res.render('calculator');
+});
 
+app.post('/calculator', function(req, res){
+	var a = parseInt(req.body.a);
+	var b = parseInt(req.body.b);
+
+	var ketqua = a + b;
+	res.render('calculator', {result: ketqua});
+
+});
 app.listen(3000, function (){
 	console.log("Server is listening in port 3000....")
 });
